76e796f92c0c8b56ab2d991c64f2a593ffb09c81
Michelline Tran <michellinet@procore.com>
2019-09-17 15:42:30 -0500

    Set up Xcode project

---
+++ Foodnotes/Foodnotes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Foodnotes
+//
+//  Created by Michelline Tran on 9/17/19.
+//  Copyright © 2019 Michelline Tran. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
