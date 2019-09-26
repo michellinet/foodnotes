262fc0681d2d2e6fdbec1af911afcd915a020502
Michelline Tran <michellinet@procore.com>
2019-09-26 16:03:58 -0500

    Set up Firebase (#2)

--- Foodnotes/Foodnotes/AppDelegate.swift
+++ Foodnotes/Foodnotes/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Michelline Tran. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
