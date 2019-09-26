149a0f6b1370fb419a08979d0223c990bd546310
Michelline Tran <michellinet@procore.com>
2019-09-26 14:57:10 -0500

    Set up Xcode project & Appsody (#1)
    * Set up Xcode project
    
    * Set up Appsody

---
+++ Foodnotes/Foodnotes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Foodnotes
+//
+//  Created by Michelline Tran on 9/17/19.
+//  Copyright © 2019 Michelline Tran. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
